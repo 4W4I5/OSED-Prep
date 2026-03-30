4cad937624a5f3f996cc67a1537721e43076d8f8
Awais Shahid (Work) <i212764@nu.edu.pk>
2026-03-30 15:08:51 +0500

    scaffold

--- Chapter11 - ASLRBypass/sideProject/Network.py
+++ Chapter11 - ASLRBypass/sideProject/Network.py
@@ -1,1 +1,17 @@
 # This is the class to send over buffer/process responses
+
+class Connection:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = None
+    def connect(self):
+        # code to connect to the server
+        pass
+    def send(self, data):
+        # code to send data to the server
+        pass    
+    def receive(self):
+        # code to receive data from the server
+        pass
+    
